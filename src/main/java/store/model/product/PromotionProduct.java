efc9bd8cef364581504f749162169cccc6550a7e
choi <nippert@naver.com>
2024-11-10 00:16:43 +0900

    refactor: 프로모션 enum 적용

--- src/main/java/store/model/product/PromotionProduct.java
+++ src/main/java/store/model/product/PromotionProduct.java
@@ -1,14 +1,17 @@
 package store.model.product;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import store.model.promotion.PromotionType;
+
 import java.time.LocalDate;
 
 public class PromotionProduct extends Product {
     private final String promotionName;
-    private final String promotionType;
+    private final PromotionType promotionType;
     private final LocalDate start_date;
     private final LocalDate end_date;
 
-    private PromotionProduct(String name, int price, int quantity, String promotionName, String promotionType, LocalDate start_date, LocalDate end_date) {
+    private PromotionProduct(String name, int price, int quantity, String promotionName, PromotionType promotionType, LocalDate start_date, LocalDate end_date) {
         super(name, price, quantity);
         this.promotionName = promotionName;
         this.promotionType = promotionType;
@@ -16,8 +19,13 @@ public class PromotionProduct extends Product {
         this.end_date = end_date;
     }
 
-    public static PromotionProduct createPromotionProduct(String name, int price, int quantity, String promotionName, String promotionType, LocalDate start_date, LocalDate end_date) {
+    public static PromotionProduct createPromotionProduct(String name, int price, int quantity, String promotionName, PromotionType promotionType, LocalDate start_date, LocalDate end_date) {
         return new PromotionProduct(name, price, quantity, promotionName, promotionType, start_date, end_date);
+    }
+
+    public boolean hasQuantity() {
+        LocalDate localDate = DateTimes.now().toLocalDate();
+        return localDate.isAfter(start_date) && localDate.isBefore(end_date) && quantity > QUANTITY_MINIMUM;
     }
 
     @Override
