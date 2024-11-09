cf01cb7c78684b3f4ed6cb22a4bb8b48feca96d6
choi <nippert@naver.com>
2024-11-10 00:34:08 +0900

    refactor: 사용자로부터 입력을 받을 때 유효성 검사하기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,20 @@
 package view;
 
+import store.model.Store;
+import view.dto.ProductDto;
+import view.validator.InputValidator;
+
+import java.util.List;
 import java.util.function.Supplier;
 
 public class InputView {
 
     private final Supplier<String> reader;
+    private final InputValidator inputValidator;
 
-    public InputView(Supplier<String> reader) {
+    public InputView(Supplier<String> reader, InputValidator inputValidator) {
         this.reader = reader;
+        this.inputValidator = inputValidator;
     }
 
     private <T> T repeatLoop(Supplier<T> inputFunction) {
@@ -27,50 +34,55 @@ public class InputView {
         System.out.print(System.lineSeparator());
     }
 
-    public String readItem() {
+    public List<ProductDto> readItem(Store store) {
         return repeatLoop(() -> {
             System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
             String item = reader.get();
             printLineSeperate();
-            return item;
+            List<ProductDto> items = inputValidator.validateItemInput(store, item);
+            return items;
         });
     }
 
-    public String readAddItem(String item, int quantity) {
+    public boolean readAddItem(String item, int quantity) {
         return repeatLoop(() -> {
             System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)", item, quantity);
             printLineSeperate();
             String addItemAnswer = reader.get();
             printLineSeperate();
-            return addItemAnswer;
+            boolean answer = inputValidator.validateAnswer(addItemAnswer);
+            return answer;
         });
     }
 
-    public String readPartialPayment(String item, int quantity) {
+    public boolean readPartialPayment(String item, int quantity) {
         return repeatLoop(() -> {
             System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", item, quantity);
             printLineSeperate();
             String partialPayment = reader.get();
             printLineSeperate();
-            return partialPayment;
+            boolean answer = inputValidator.validateAnswer(partialPayment);
+            return answer;
         });
     }
 
-    public String readMemberDiscount() {
+    public boolean readMemberDiscount() {
         return repeatLoop(() -> {
             System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
             String discountAnswer = reader.get();
             printLineSeperate();
-            return discountAnswer;
+            boolean answer = inputValidator.validateAnswer(discountAnswer);
+            return answer;
         });
     }
 
-    public String readMorePurchase() {
+    public boolean readMorePurchase() {
         return repeatLoop(() -> {
             System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
             String morePurchaseAnswer = reader.get();
             printLineSeperate();
-            return morePurchaseAnswer;
+            boolean answer = inputValidator.validateAnswer(morePurchaseAnswer);
+            return answer;
         });
     }
 }
