8f0775cf7984900b6051f6d5431be73ad610506f
choi <nippert@naver.com>
2024-11-10 15:47:22 +0900

    fix: 할인은 중복으로 되지 않는다

--- src/main/java/store/service/DiscountService.java
+++ src/main/java/store/service/DiscountService.java
@@ -3,6 +3,8 @@ package store.service;
 import store.model.ReceiptContent;
 import store.model.Receipt;
 
+import java.util.Map;
+
 public class DiscountService {
     private static final int NO_MEMBERSHIP_DISCOUNT = 0;
 
@@ -21,8 +23,20 @@ public class DiscountService {
 
 
     public int calculateMemberShipDiscount(Receipt receipt, boolean answer) {
-        int totalMoney = receipt.calculateTotalMoney();
+        //프로모션 할인을 받지 않은 상품에 대해서만 할인을 적용해야 한다
+        //즉,purchase에 있는 상품들 중에 gift에 없는 것들만 할인을 적용해야 한다
+        Map<String, ReceiptContent> gift = receipt.getGift();
+        Map<String, ReceiptContent> purchase = receipt.getPurchase();
         if (answer) {
+            int totalMoney = 0;
+
+            // purchase에 있지만 gift에 없는 상품들만 처리
+            for (Map.Entry<String, ReceiptContent> entry : purchase.entrySet()) {
+                if (!gift.containsKey(entry.getKey())) {
+                    totalMoney += entry.getValue().getPrice() * entry.getValue().getQuantity();
+                }
+            }
+
             int memberShipDiscount = receipt.calculateMemberShipDiscount(totalMoney);
             return memberShipDiscount;
         }
@@ -33,12 +47,22 @@ public class DiscountService {
     public int calculatePurchase(Receipt receipt, boolean answer) {
         int totalMoney = receipt.calculateTotalMoney();
         int promotionDiscount = receipt.calculateEventDiscount();
-        int memberShipDiscount = receipt.calculateMemberShipDiscount(totalMoney);
-        int purchase;
+        Map<String, ReceiptContent> gift = receipt.getGift();
+        Map<String, ReceiptContent> purchase = receipt.getPurchase();
+        int tmpMoney = 0;
+
+        // purchase에 있지만 gift에 없는 상품들만 처리
+        for (Map.Entry<String, ReceiptContent> entry : purchase.entrySet()) {
+            if (!gift.containsKey(entry.getKey())) {
+                tmpMoney += entry.getValue().getPrice() * entry.getValue().getQuantity();
+            }
+        }
+        int memberShipDiscount = receipt.calculateMemberShipDiscount(tmpMoney);
+        int purchaseMoney;
 
         if (answer) {
-            purchase = receipt.calculatePurchase(totalMoney, promotionDiscount, memberShipDiscount);
-            return purchase;
+            purchaseMoney = receipt.calculatePurchase(totalMoney, promotionDiscount, memberShipDiscount);
+            return purchaseMoney;
         }
         return receipt.calculatePurchase(totalMoney, promotionDiscount, NO_MEMBERSHIP_DISCOUNT);
     }
