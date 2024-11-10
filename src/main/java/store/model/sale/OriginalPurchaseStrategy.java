7feb434bffe728a701979e6223bbe883fd4d01b7
choi <nippert@naver.com>
2024-11-10 14:12:19 +0900

    refactor: 정가로 계산하는 로직 작게 분리

--- src/main/java/store/model/sale/OriginalPurchaseStrategy.java
+++ src/main/java/store/model/sale/OriginalPurchaseStrategy.java
@@ -4,6 +4,8 @@ import store.model.Receipt;
 import store.model.Store;
 
 public class OriginalPurchaseStrategy implements SaleStrategy {
+    private static final int PROMOTION_NOT_EXIST = 0;
+
     @Override
     public void execute(Store store, Receipt receipt, String productName, int quantity, Object... options) {
         int partial = (int) options[0];
@@ -13,22 +15,40 @@ public class OriginalPurchaseStrategy implements SaleStrategy {
 
     private void purchaseProductOriginal(Store store, Receipt receipt, String productName, int quantity, int partial, boolean answer) {
         if (answer) {
-            int free = store.getFreePromotion(productName, quantity - partial);
-            int promotionQuantity = store.getPromotionProduct().get(productName).getQuantity();
-            int notFree = quantity - promotionQuantity;
-
-            receipt.addPurchase(productName, store.getPromotionProduct().get(productName).getPrice(), quantity);
-            receipt.addGift(productName, store.getPromotionProduct().get(productName).getPrice(), free);
-            store.updateStoreGeneral(productName, notFree);
-            store.updateStorePromotion(productName, promotionQuantity);
-        } else {
-            quantity -= partial;
-            int free = store.getFreePromotion(productName, quantity);
-            int notFree = quantity - free;
-
-            receipt.addPurchase(productName, store.getPromotionProduct().get(productName).getPrice(), quantity);
-            receipt.addGift(productName, store.getPromotionProduct().get(productName).getPrice(), free);
-            store.updateStorePromotion(productName, notFree);
+            handlePromotionPurchase(store, receipt, productName, quantity, partial);
+            return;
         }
+
+        handleOriginalPurchase(store, receipt, productName, quantity, partial);
+    }
+
+    private void handlePromotionPurchase(Store store, Receipt receipt, String productName, int quantity, int partial) {
+        int free = store.getFreePromotion(productName, quantity - partial);
+        int promotionQuantity = store.getPromotionProduct().get(productName).getQuantity();
+        int notFree = quantity - promotionQuantity;
+
+        updateToReceipt(store, receipt, productName, quantity, free);
+        updateToStore(store, receipt, productName, promotionQuantity, notFree);
+    }
+
+    private void handleOriginalPurchase(Store store, Receipt receipt, String productName, int quantity, int partial) {
+        quantity -= partial;
+        int free = store.getFreePromotion(productName, quantity);
+        int notFree = quantity - free;
+
+        updateToReceipt(store, receipt, productName, quantity, free);
+        updateToStore(store, receipt, productName, PROMOTION_NOT_EXIST, notFree);
+    }
+
+    private void updateToReceipt(Store store, Receipt receipt, String productName, int quantity, int free) {
+        receipt.addPurchase(productName, store.getPromotionProduct().get(productName).getPrice(), quantity);
+        receipt.addGift(productName, store.getPromotionProduct().get(productName).getPrice(), free);
+    }
+
+    private void updateToStore(Store store, Receipt receipt, String productName, int promotionQuantity, int notFree) {
+        if(promotionQuantity != PROMOTION_NOT_EXIST) {
+            store.updateStorePromotion(productName, promotionQuantity);
+        }
+        store.updateStoreGeneral(productName, notFree);
     }
 }
